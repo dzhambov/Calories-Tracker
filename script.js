08fc9eb1ae8a1a1b614a9057d4a018aa66e69914
Hristo <owner@iMac.local>
2020-11-14 16:15:03 -0800

    add delete and clear from local storage.

--- script.js
+++ script.js
@@ -35,6 +35,30 @@ const StorageCtrl = (function() {
         items = JSON.parse(localStorage.getItem('items'));
       }
       return items;
+    },
+
+    updateItemStorage: function(updatedItem) {
+      let items = JSON.parse(localStorage.getItem('items'));
+
+      items.forEach(function(item, index) {
+        if(updatedItem.id === item.id) {
+          items.splice(index, 1, updatedItem);
+        }
+      });
+      localStorage.setItem('items', JSON.stringify(items));
+    },
+    deleteItemFromStorage: function(id) {
+      let items = JSON.parse(localStorage.getItem('items'));
+
+      items.forEach(function(item, index) {
+        if(id === item.id) {
+          items.splice(index, 1);
+        }
+      });
+      localStorage.setItem('items', JSON.stringify(items));
+    },
+    clearItemsFromStorage: function() {
+      localStorage.removeItem('items');
     }
   }
 })();
@@ -396,6 +420,9 @@ const App = (function(ItemCtrl, StorageCtrl, UICtrl) {
     //  Add total calories to UI
     UICtrl.showTotalCalories(totalCalories);
 
+    // Update local storage
+    StorageCtrl.updateItemStorage(updatedItem);
+
     UICtrl.clearEditState();
 
     e.preventDefault();
@@ -418,6 +445,9 @@ const App = (function(ItemCtrl, StorageCtrl, UICtrl) {
     //  Add total calories to UI
     UICtrl.showTotalCalories(totalCalories);
 
+    // Delete from LS
+    StorageCtrl.deleteItemFromStorage(currentItem.id);
+
     UICtrl.clearEditState();
 
     e.preventDefault();
@@ -436,6 +466,9 @@ const App = (function(ItemCtrl, StorageCtrl, UICtrl) {
 
     // Remove from UI
     UICtrl.removeItems();
+
+    // Clear from LS
+    StorageCtrl.clearItemsFromStorage();
 
     // Hide the UL
     UICtrl.hideList();
